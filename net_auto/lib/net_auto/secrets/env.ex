7e394db4c71f11b029a308eb20ab7977ec3ac4fb
Martin <thommesen.m@icloud.com>
2025-11-08 19:47:21 +0100

    feat(ws03): add env secrets adapter

---
+++ net_auto/lib/net_auto/secrets/env.ex
@@ -0,0 +1,73 @@
+defmodule NetAuto.Secrets.Env do
+  @moduledoc """
+  Fetches credentials from OS environment variables.
+
+  Expected keys per `cred_ref`:
+  - `NET_AUTO_<REF>_USERNAME`
+  - `NET_AUTO_<REF>_PASSWORD`
+  - `NET_AUTO_<REF>_PRIVKEY`
+  - `NET_AUTO_<REF>_PRIVKEY_BASE64`
+  - `NET_AUTO_<REF>_PASSPHRASE`
+  """
+
+  @behaviour NetAuto.Secrets
+
+  alias NetAuto.Secrets.Credential
+
+  @impl true
+  def fetch(cred_ref, opts \\ []) when is_binary(cred_ref) do
+    prefix = Keyword.get(opts, :prefix, "NET_AUTO")
+    slug = normalize(cred_ref)
+    key = prefix <> "_" <> slug
+
+    username = Keyword.get(opts, :username) || env(key <> "_USERNAME")
+    password = env(key <> "_PASSWORD")
+    passphrase = env(key <> "_PASSPHRASE")
+    private_key = resolve_private_key(key)
+
+    credentials_present? = Enum.any?([password, private_key], &(&1 && &1 != ""))
+
+    if credentials_present? do
+      credential = %Credential{
+        cred_ref: cred_ref,
+        username: username,
+        password: password,
+        private_key: private_key,
+        passphrase: passphrase,
+        metadata: %{source: :env}
+      }
+
+      {:ok, credential}
+    else
+      {:error, :missing_secret}
+    end
+  end
+
+  defp env(name) do
+    case System.get_env(name) do
+      nil -> nil
+      value when value == "" -> nil
+      value -> value
+    end
+  end
+
+  defp normalize(ref) do
+    ref
+    |> String.upcase()
+    |> String.replace(~r/[^A-Z0-9]/, "_")
+  end
+
+  defp resolve_private_key(base_key) do
+    case {env(base_key <> "_PRIVKEY"), env(base_key <> "_PRIVKEY_BASE64")} do
+      {key, nil} when is_binary(key) -> key
+      {nil, encoded} when is_binary(encoded) ->
+        case Base.decode64(encoded) do
+          {:ok, decoded} -> decoded
+          :error -> nil
+        end
+
+      {key, _encoded} when is_binary(key) -> key
+      _ -> nil
+    end
+  end
+end
--- net_auto/test/net_auto/secrets_test.exs
+++ net_auto/test/net_auto/secrets_test.exs
@@ -3,6 +3,7 @@ defmodule NetAuto.SecretsTest do
 
   alias NetAuto.Secrets
   alias NetAuto.Secrets.Credential
+  alias NetAuto.Secrets.Env
 
   defmodule StubAdapter do
     @behaviour NetAuto.Secrets
@@ -38,5 +39,49 @@ defmodule NetAuto.SecretsTest do
                     %{cred_ref: "TEST", result: :ok}}
 
     :telemetry.detach("secrets-test")
+  end
+
+  describe "Env adapter" do
+    setup do
+      keys = [
+        "NET_AUTO_LAB_DEFAULT_USERNAME",
+        "NET_AUTO_LAB_DEFAULT_PASSWORD",
+        "NET_AUTO_LAB_DEFAULT_PRIVKEY",
+        "NET_AUTO_LAB_DEFAULT_PRIVKEY_BASE64",
+        "NET_AUTO_LAB_DEFAULT_PASSPHRASE"
+      ]
+
+      snapshot =
+        Enum.map(keys, fn key ->
+          {key, System.get_env(key)}
+        end)
+
+      on_exit(fn ->
+        Enum.each(snapshot, fn
+          {key, nil} -> System.delete_env(key)
+          {key, value} -> System.put_env(key, value)
+        end)
+      end)
+
+      Enum.each(keys, &System.delete_env/1)
+      :ok
+    end
+
+    test "fetch/2 returns password credentials" do
+      System.put_env("NET_AUTO_LAB_DEFAULT_PASSWORD", "hunter2")
+
+      assert {:ok, %Credential{password: "hunter2", private_key: nil}} = Env.fetch("lab-default")
+    end
+
+    test "fetch/2 decodes base64 private key" do
+      key = "----BEGIN KEY----\nabc123"
+      System.put_env("NET_AUTO_LAB_DEFAULT_PRIVKEY_BASE64", Base.encode64(key))
+
+      assert {:ok, %Credential{private_key: ^key}} = Env.fetch("Lab Default")
+    end
+
+    test "fetch/2 errors when neither password nor key present" do
+      assert {:error, :missing_secret} = Env.fetch("missing")
+    end
   end
 end
